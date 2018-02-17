9628569fc54e1f4e1c83bccced564d2f39e62d4b
doushan <dusyant3993@gmail.com>
2018-02-17 23:54:13 +0400

    Updated code

--- spreadsheet.py
+++ spreadsheet.py
@@ -7,11 +7,27 @@ scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name('secret.json', scope)
 client = gspread.authorize(creds)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("PBIO EOY - Music list").sheet1
+#fine a workbook and use it name, done forget to share the work book with the
+#client email address in order to have access to it.
+sheet = client.open("Python with excel").sheet1
 
-# Extract and print all of the values
 pp = pprint.PrettyPrinter()
-list_of_hashes = sheet.get_all_records()
-pp.pprint(list_of_hashes)
+
+#this will print all records
+result = sheet.get_all_records()
+
+#this will print row value for a particular row
+result1 = sheet.row_values(5)
+
+#this will print coloum value for a particular coloum
+result2 = sheet.col_values(4)
+
+#this will print cell value for a particular cell
+result3 = sheet.cell(5,5).value
+pp.pprint(result3)
+
+#this will change the result of a particular cell
+sheet.update_cell(5, 5, 'MALE')
+result4 = sheet.cell(5, 5).value
+
+pp.pprint(result4)
